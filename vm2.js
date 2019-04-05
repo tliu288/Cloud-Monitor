2eefb513640043c0c38f1ef8a4b59c92eb7760dd
tliu288 <24960949+tliu288@users.noreply.github.com>
2019-04-04 20:45:30 -0400

    Add files via upload

---
+++ vm2.js
@@ -0,0 +1,66 @@
+const Express = require("express");
+var fs = require('fs');
+var app = Express();
+
+const port = process.env.PORT || 8081;
+const server = app.listen(port, function(){
+    console.log('listening on port ' + port);
+});
+
+var logdata;
+
+// Start VM
+app.post("/instance/start/:id", (request, response) => {
+    logdata = "Virtual machine: [" + request.params.id + "]     Event: Start      TimeStamp : " + new Date() + "\n";
+    console.log(logdata);
+
+    fs.appendFile("log.txt", logdata, function(err, logdata) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+
+});
+
+// Stop VM
+app.post("/instance/start/:id", (request, response) => {
+    logdata = "Virtual machine: [" + request.params.id + "]     Event: Stop      TimeStamp : "+ new Date() + "\n";
+    console.log(logdata);
+
+    fs.appendFile("log.txt", logdata, function(err, logdata) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+
+});
+
+app.get("/instance/upgrade/:id", (request, response) => {
+    logdata = "Virtual machine: [" + request.params.id + "]     Event: Upgrade      TimeStamp : "+ new Date() + "\n";
+    console.log(logdata);
+
+    fs.appendFile("log.txt", logdata, function(err, logdata) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+
+});
+
+app.get("/instance/downgrade/:id", (request, response) => {
+    logdata = "Virtual machine: [" + request.params.id + "]     Event: Downgrade      TimeStamp : "+ new Date() + "\n";
+    console.log(logdata);
+
+    fs.appendFile("log.txt", logdata, function(err, logdata) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+});
+
+app.get("/instance/delete/:id", (request, response) => {
+    logdata = "Virtual machine: [" + request.params.id + "]     Event: Delete     s TimeStamp : "+ new Date() + "\n";
+    console.log(logdata);
+
+    fs.appendFile("log.txt", logdata, function(err, logdata) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+})
+
