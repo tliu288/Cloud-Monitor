46f861cfcaf8ee547f3434473a52d6a12c3a57a1
tliu288 <24960949+tliu288@users.noreply.github.com>
2019-04-04 22:24:40 -0400

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,48 @@
+import socket
+import time
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+while 1:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	result = sock.connect_ex(('0.0.0.0',8081))
+	if result == 0:
+  	 print ("Port is in use")
+	else:
+  	 print ("Port is not in use")
+  	 mail_content = '''Hello,
+  	 The monitor has crashed. Please restart.
+  	 '''
+  	 #The mail addresses and password
+  	 sender_address = 'leonliu.elite@gmail.com'
+  	 sender_pass = 'ilovekadaj'
+  	 receiver_address = 'leonliu288@gmail.com'
+  	 #Setup the MIME
+  	 message = MIMEMultipart()
+  	 message['From'] = sender_address
+  	 message['To'] = receiver_address
+  	 message['Subject'] = 'The monitor has crashed. Please restart.'
+  	 #The subject line
+  	 message.attach(MIMEText(mail_content, 'plain'))
+  	 #Create SMTP session for sending the mail
+  	 session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+  	 session.starttls() #enable security
+  	 session.login(sender_address, sender_pass) #login with mail_id and password
+  	 text = message.as_string()
+  	 session.sendmail(sender_address, receiver_address, text)
+  	 session.quit()
+  	 print('The monitor alert has Sent')
+
+	sock.close()
+	time.sleep(5)
+
